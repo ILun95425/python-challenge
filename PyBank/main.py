dfc18483f58ba6c1b075b9858884e4bfdc3fe369
ILun95425 <149023115+ILun95425@users.noreply.github.com>
2024-04-25 00:07:38 -0400

    upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,49 +1,86 @@
 import os
-import pandas as pd
+import csv
 
-fa= pd.read_csv('PyBank/Resources/budget_data.csv')
 
-total_months= fa['Date'].nunique()
+filein = r"C:\Users\ilun1\OneDrive\桌面\GitHub\python-challenge\PyBank\Resources\budget_data.csv"
 
-total= fa['Profit/Losses'].sum()
 
-fa['Change']= fa['Profit/Losses'].diff()
+# define required variables
 
-average_change= fa['Change'].mean()
+months = []
+changes_pNl =[]
+net_pNl = 0
+avg_change = 0
+counter_month = 0
+grt_loss_month = 0
+grt_profit_month = 0
+current_month_pNl = 0
+previous_month_pNl = 0
+grt_loss = 0
+grt_profit = 0
 
-max_increase_month= fa.loc[fa['Change'].idxmax(), 'Date']
 
-max_increase_amount= fa['Change'].max()
+with open(filein, 'r') as input_data:
+    read = csv.reader(input_data, delimiter= ',')
 
-max_decrease_month= fa.loc[fa['Change'].idxmin(), 'Date']
+# skip the header
 
-max_decrease_amount= fa['Change'].min()
+    header = next(read)
+    
 
-#print
+# loop csv
+
+    for rows in read:
+        months.append(rows[0])     
+     
+        net_pNl += int(rows[1])    
+        
+        counter_month  += 1        
+        
+        current_month_pNl = int(rows[1])      
+       
+        if(counter_month == 1) :
+            previous_month_pNl = current_month_pNl       
+            
+        else :                                           
+            change = current_month_pNl - previous_month_pNl              
+            previous_month_pNl = current_month_pNl                       
+            
+            changes_pNl.append(change)                     
+               
+
+avg_change = round(sum(changes_pNl)/(counter_month-1),2)   
+grt_profit = max(changes_pNl)                             
+grt_profit_month = months[changes_pNl.index(grt_profit)+1]   
+grt_loss = min(changes_pNl)                               
+grt_loss_month = months[changes_pNl.index(grt_loss)+1] 
+
+# print method 1
 
 print("Financial Analysis")
 print("---------------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${total}")
-print(f"Average Change: ${average_change:.2f}")
-print(f"Greatest Increase in Profits: {max_increase_month} (${max_increase_amount:.2f})")
-print(f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease_amount:.2f})")
+print(f"Total Months: {counter_month}")
+print(f"Total: ${net_pNl}")
+print(f"Average Change: ${avg_change:.2f}")
+print(f"Greatest Increase in Profits: {grt_profit_month} (${grt_profit})")
+print(f"Greatest Decrease in Profits: {grt_loss_month} (${grt_loss})")
 
 
-#addition
+# output text file method 1
 
 results= (
     "Financial Analysis\n"
     "---------------------------------\n"
-    f"Total Months: {total_months}\n"
-    f"Total: ${total}\n"
-    f"Average Change: ${average_change:.2f}\n"
-    f"Greatest Increase in Profits: {max_increase_month} (${max_increase_amount:.2f})\n"
-    f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease_amount:.2f})"
+    f"Total Months: {counter_month}\n"
+    f"Total: ${net_pNl}\n"
+    f"Average Change: ${avg_change:.2f}\n"
+    f"Greatest Increase in Profits: {grt_profit_month} (${grt_profit:.2f})\n"
+    f"Greatest Decrease in Profits: {grt_loss_month} (${grt_loss:.2f})\n"
 )
 
 print(results)
 
+output_folder = r"C:\Users\ilun1\OneDrive\桌面\GitHub\python-challenge\PyBank"
+output_file = os.path.join(output_folder, 'Financial Analysis.txt')
 
-output_file= 'Financial Analysis.txt'
 with open(output_file, 'w') as file: file.write(results)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,51 +1,80 @@
 import os
-import pandas as pd
+import csv
 
-er= pd.read_csv('PyPoll/Resources/election_data.csv')
 
-total_votes= er['Ballot ID'].nunique()
+filein = r"C:\Users\ilun1\OneDrive\桌面\GitHub\python-challenge\PyPoll\Resources\election_data.csv"
 
-candidates= er['Candidate'].unique()
 
-candidate_results= {}
+# define required variables
 
-for candidate in candidates:
+candidatesNvotes= {}
+total_votes = 0
+
+with open(filein, 'r') as input_data:
+    read = csv.reader(input_data, delimiter= ',')
+
+# skip the header
+
+    header = next(read)
+
+# loop csv
     
-    candidate_votes= er[er['Candidate'] == candidate]['Ballot ID'].count()
-    percentage= (candidate_votes / total_votes) * 100
-    candidate_results[candidate]= {
-        'Votes': candidate_votes, 'Percentage': percentage
-        }
-    
-winner= max(candidate_results, key=lambda x: candidate_results[x]['Votes'])
+    for row in read:
+        
+        if not row[2] in candidatesNvotes.keys():  
+            candidatesNvotes[row[2]] = 1          
+                                                                                                     
+        else:                                      
+            candidatesNvotes[row[2]] += 1
+
+total_votes = sum(candidatesNvotes.values())
+
+candidate_results = {}
+
+for candidate, votes in candidatesNvotes.items():
+    vote_percent = votes / total_votes
+    candidate_results[candidate] = {'Votes': votes, 'Percentage': vote_percent}
+
+winner = max(candidate_results, key=lambda x: candidate_results[x]['Votes'])
+
+#     for row in read:
+#         total_votes += 1
+#         candidate = row[2]  
+#         if candidate not in candidates:
+#             candidates.append(candidate)
+#             candidate_results[candidate] = {'Votes': 0, 'Percentage': 0}
+#         candidate_results[candidate]['Votes'] += 1
 
 
-#print
+# winner = max(candidate_results, key=lambda x: candidate_results[x]['Votes'])
 
 
+
+# print method 2
 
 print("Election Results")
 print("---------------------------------")
 print(f"Total Votes: {total_votes}")
 print("---------------------------------")
-for candidate in candidates:
-    print(f"{candidate}: {candidate_results[candidate]['Percentage']:.3f}% ({candidate_results[candidate]['Votes']})")
+for candidate, results in candidate_results.items():
+    print(f"{candidate}: {results['Percentage']:.3f}% ({results['Votes']})")
 print("---------------------------------")
 print(f"Winner: {winner}")
 print("---------------------------------")
 
 
-#addition
+# output text file method 2
 
-output_file= 'Election Results.txt'
+output_folder = r"C:\Users\ilun1\OneDrive\桌面\GitHub\python-challenge\PyPoll"
+fileout = os.path.join(output_folder, 'Election Results.txt')
 
-with open(output_file, 'w') as file:
+with open(fileout, 'w') as file:
     file.write("Election Results\n")
     file.write("---------------------------------\n")
-    file.write(f"Total Votes: {total_votes}\n")
+    file.write("Total Votes: {total_votes}")
     file.write("---------------------------------\n")
-    for candidate in candidates:
-        file.write(f"{candidate}: {candidate_results[candidate]['Percentage']:.3f}% ({candidate_results[candidate]['Votes']})\n")
+    for candidate, results in candidate_results.items():
+        file.write(f"{candidate}: {results['Percentage']:.3f}% ({results['Votes']})\n")
     file.write("---------------------------------\n")
     file.write(f"Winner: {winner}\n")
     file.write("---------------------------------\n")
